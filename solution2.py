490553ad09f29a174956b93cbf745a117402c9eb
Greg Smith <variablegreg@gmail.com>
2023-07-14 13:15:19 -0700

    secret update more

--- solution2.py
+++ solution2.py
@@ -4,34 +4,41 @@ def solution(x, y):
     import numpy as np
     solution = ""
 
-    arr = np.tri(5,5)
 
-    print(arr[x, y])
-
-    #print(arr[2])
     
+    def getVerticleRowNum(thatRow):
+        print("The row detected is", thatRow)
+        #return thatRow + (thatRow - 1)
+        print(thatRow + (thatRow - 1))
+
+    getVerticleRowNum(y)
+
     laIndex = 1
+    vertRow = 19
     arrI = 1
     laArr = []
     #ok, sort of got this one going but it's only part 1
-    while laIndex < 1000:
-        laArr.append(int((laIndex * (laIndex + 1))/2 ))
+    while laIndex < 20:
+        laArr.append(int((laIndex * (laIndex + vertRow))/2)+36)
         laIndex += 1
 
     print(laArr)
         
 
 
-# 46
-# 37 47
-# 29 38 48
-# 22 30 39 49 
-# 16 23 31 40 50
-# 11 17 24 32 41 51 
-# 7 12 18 25 33 42 52
-# 4 8 13 19 26 34 43 53 
-# 2 5 9 14 20 27 35 44 54
-# 1 3 6 10 15 21 28 36 45 55
+
+
+
+# 46                                  row 19 (+36 needed)
+# 37 47                               row 17 (+28 needed)
+# 29 38 48                            row 15 (+21 needed)
+# 22 30 39 49                         row 13 (+15 needed)
+# 16 23 31 40 50                      row 11 (+10 needed)
+# 11 17 24 32 41 51                   row 9 (+6 needed)
+# 7 12 18 25 33 42 52                 row 7 (+3 needed)
+# 4 8 13 19 26 34 43 53               row 5 (+1 needed)
+# 2 5 9 14 20 27 35 44 54             row 3 (no mod needed)
+# 1 3 6 10 15 21 28 36 45 55          row 1 (no mod needed)
 
 # This can be further simplified as [n(n+1)]/2
 
