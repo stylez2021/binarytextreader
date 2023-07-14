f9b77360d0e098647ead6283bd2ffc0b66b24414
Greg Smith <variablegreg@gmail.com>
2023-07-13 19:44:13 -0700

    Figured it out thanks to using the bitarray library

--- binreader.py
+++ binreader.py
@@ -1,16 +1,16 @@
 #binreader
 
-# python file to read a simple text file containing binary code and output it's characters to the screen
-
-# first we'll just set up the skeleton and print functions to remind ourselfs
-
-# let's take some args and print out the name of the file entered
+#python file to read a simple text file containing binary code and output it's characters to the screen
+#first we'll just set up the skeleton and print functions to remind ourselfs
 
 import sys
 import io
+from bitarray import bitarray
+
 
 print("Welcome to BinReader, hope you enjoy your stay")
 
+#let's take some args and print out the name of the file entered
 #this should be the file indicated by the first CLI argument, no error checking for now
 targetFile = sys.argv[1]
 print("File to scan is", targetFile)
@@ -18,12 +18,15 @@ print("File to scan is", targetFile)
 #open up the file for use in python
 with open(targetFile, "r") as readFile:
     fileText = (readFile.read())
-    print(fileText)
+    print(fileText.split())
 
-#now store the sets of binary numbers into a list
-#close but ... need to separated them by spaces how do I do that?
-textList = []
-for i in fileText:
-    textList.append(i)
 
-print(textList)
+# Ok now, I can print the list just to see it and make sure but this part isn't necessary, already printed above so commenting out.
+#for i in fileText.split():
+#    print(i)
+
+#convert and print
+bts = bitarray(fileText)
+ASCS = bts.tobytes().decode('ascii')
+print("\nThe message should be:\n")
+print(ASCS + "\n")
