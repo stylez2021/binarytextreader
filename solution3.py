08a72295f637c122fa1555d10df6097aafe05d3b
Greg Smith <variablegreg@gmail.com>
2023-07-14 22:00:23 -0700

    10pm save

--- solution3.py
+++ solution3.py
@@ -1,23 +1,32 @@
 def solution(xs):
     #my code here
+    import numpy
     powerArray = []
 
     print(xs)
     for num in xs:
-        print(num)
+        #print(num)
         if num != 0:
             powerArray.append(num)
 
-    print(powerArray)
+    #print(powerArray)
 
     #times the numbers in the powerArray by themselfs and print the total?
+    #The positive numbers seem to be passing, how do I deal with negatives?  Math is hard
     result = 1 
+    result2 = numpy.prod(powerArray)
     for x in powerArray:
         result = result * x
-    print(result)
+    print(str(result))
+    print(str(result2))
 
+# Input:
+# solution.solution([-2, -3, 4, -5])
+# Output:    60
 
-solution([2, 0, 2, 2, 0])
+# solution([2, 0, 2, 2, 0]) Working
+solution([-2, -3, 4, -5])
+solution([-2, 1, -8])
 
 # You need to figure out which sets of panels in any given array you can take offline to repair while still maintaining the maximum amount
 # of power output per array, and to do THAT, you'll first need to figure out what the maximum output of each array actually is.
@@ -25,6 +34,11 @@ solution([2, 0, 2, 2, 0])
 # the maximum product of some non-empty subset of those numbers. So for example, if an array contained panels with power output levels of
 # [2, -3, 1, 0, -5], then the maximum product would be found by taking the subset: xs[0] = 2, xs[1] = -3, xs[4] = -5, giving the product 2*(-3)*(-5) = 30.
 # So solution([2,-3,1,0,-5]) will be "30".
+
+# Each array of solar panels contains at least 1 and no more than 50 panels, and each panel will have a power output level whose absolute value is no
+# greater than 1000 (some panels are malfunctioning so badly that they're draining energy, but you know a trick with the panels' wave stabilizer that
+# lets you combine two negative-output panels to produce the positive output of the multiple of their power values). The final products may be very large,
+# so give the solution as a string representation of the number.
 
 
 #print(a string)
